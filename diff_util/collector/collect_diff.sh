4fcd90543392803d361d2a9ef38b77b7332eb9ab
ehdkacjswo <ehdkacjswo@gmail.com>
2024-10-30 09:30:02 +0900

    Data generated with new encoding theme (Deleted older ones)

---
+++ diff_util/collector/collect_diff.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+error_trace=/root/workspace/setup.error
+
+# Set the base directory to iterate through
+BASE_DIR=/root/workspace/data/Defects4J/baseline/
+
+# Check if the base directory is provided and exists
+if [[ -z "$BASE_DIR" || ! -d "$BASE_DIR" ]]; then
+  echo "Please provide a valid directory."
+  exit 1
+fi
+
+original_cwd=$(pwd)
+
+# Iterate through directories in the base directory
+for dir in "$BASE_DIR"/*/; do
+  # Get the directory name (basename strips off the path)
+  dir_name=$(basename "$dir")
+
+  # Use pattern matching to extract the relevant parts from the directory name
+  if [[ $dir_name =~ ^([A-Za-z]+)-([0-9]+)([a-z]?)$ ]]; then
+    # Extracted parts:
+    project="${BASH_REMATCH[1]}"  # "Closure"
+    version="${BASH_REMATCH[2]}"  # "21"
+    optional_letter="${BASH_REMATCH[3]}"  # "b" (optional)
+  else
+    echo "Directory name $dir_name does not match the pattern."
+  fi
+
+  tmpdir=/tmp/${project}-${version}b/
+  faulty_version=$(grep ^${version}, /defects4j/framework/projects/${project}/commit-db | cut -d',' -f2)
+  #source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.302-open
+
+  # Checkout the faulty version to tmpdir
+  [ -d $tmpdir ] && rm -rf $tmpdir
+  defects4j checkout -p $project -v ${version}b -w ${tmpdir}
+  if [ ! -d $tmpdir ]; then
+    echo "${project}-${version}b: [Error 1] checkout failure"
+    exit 1
+  fi
+
+  cd $tmpdir
+  defects4j export -p dir.src.classes -o dir.src.classes
+
+  # Reset the commit to actual faulty version (Contents of the classes as well)
+  if [[ "$project" = "Time" ]] && (( $version > 20 )); then
+    # Dataset doesn't contain such version
+    git reset $faulty_version
+    git checkout -- JodaTime/$(cat dir.src.classes)
+    rm -rf $(cat dir.src.classes)
+    mv JodaTime/$(cat dir.src.classes) $(cat dir.src.classes)
+  else
+    git reset $faulty_version
+    git checkout -- $(cat dir.src.classes)
+    if [ $? -ne 0 ]; then
+      echo "${project}-${version}b: [Error 2] no matching src dir"
+      exit 2
+    fi
+  fi
+  echo "Reset to the actual buggy version $faulty_version  OK"
+
+  # Collect relative diff
+  python /root/workspace/diff_util/collector/collect_diff.py -p $project -v $version
+
+  # Clean up the tmp directory
+  rm -rf $tmpdir
+  echo "Cleaning up $tmpdir"
+  cd "$original_cwd"
+done
+
+  
---
+++ diff_util/encoder/diff_encoder.py
@@ -0,0 +1,131 @@
+import os, json, argparse, pickle
+import numpy as np
+import pandas as pd
+from spiral import ronin
+from collections import Counter
+from tqdm import tqdm
+
+DIFF_DATA_DIR = '/root/workspace/data/Defects4J/diff'
+
+# Class that encodes string while expanding the vocabulary
+class Encoder():
+    def __init__(self, vocab={}):
+        self.vocab = vocab # {word : id}
+    
+    # Encode the input and list of used word index and count
+    def encode(self, text):
+        encode_res = []
+        text = ronin.split(text.strip())
+
+        for word, cnt in Counter(text).items():
+            if word in self.vocab: # Word in vocab
+                encode_res.append((self.vocab[word], cnt))
+                
+            else: # New word
+                encode_res.append((len(self.vocab), cnt))
+                self.vocab[word] = len(self.vocab)
+        
+        return encode_res
+
+# Return the sum of two encoded vectors
+def sum_encode(vec1, vec2):
+    res_dict = dict()
+
+    for id, cnt in vec1:
+        res_dict[id] = cnt
+    
+    for id, cnt in vec2:
+        res_dict[id] = res_dict.get(id, 0) + cnt
+    
+    return list(res_dict.items())
+
+# Encode the raw diff data
+# Original data : [is_addition, old_file_path, new_file_path, line_num, content]
+# Encoded data : addition / deletion {file_path : sum(content_encode)}
+# Encoded data is list of (non_zero_index, non_zero_data)
+def encode_pid(pid):
+    project_dir = os.path.join(DIFF_DATA_DIR, pid)
+    encoder = Encoder()
+
+    # Iterate through commits
+    for commit in tqdm(os.listdir(project_dir)):
+        commit_dir = os.path.join(project_dir, commit)
+        
+        # Consider only commit directories (Possible vocab.pkl)
+        if not os.path.isdir(commit_dir):
+            continue
+
+        # Iterate through files
+        for filename in os.listdir(commit_dir):
+            cur_path = os.path.join(commit_dir, filename)
+            
+            # Consider only diff info (Possible ./encode/)
+            if not os.path.isfile(cur_path):
+                continue
+
+            if cur_path.endswith('.csv'): # Diff data
+                diff_df = pd.read_csv(cur_path)
+                addition_dict = dict()
+                deletion_dict = dict()
+
+                for _, row in diff_df.iterrows(): # [is_addition, old_file_path, new_file_path, line_num, content]
+                    file_path = str(row['new_file_path'])
+                    file_path_encode = tuple(encoder.encode(file_path)) # Tuple to use it as key
+                    content_encode = encoder.encode(str(row['content']))
+
+                    if row['is_addition']:
+                        addition_dict[file_path_encode] = sum_encode(addition_dict.get(file_path_encode, []), content_encode)
+                    
+                    else:
+                        if not file_path.endswith('.java'): # File deleted
+                            file_path_encode = tuple(encoder.encode(str(row['old_file_path'])))
+                        deletion_dict[file_path_encode] = sum_encode(deletion_dict.get(file_path_encode, []), content_encode)
+                
+                # Save the encoded data
+                if os.path.exists(os.path.join(commit_dir, 'encode/addition_encode.pkl')):
+                    os.remove(os.path.join(commit_dir, 'encode/addition_encode.pkl'))
+                if os.path.exists(os.path.join(commit_dir, 'encode/deletion_encode.pkl')):
+                    os.remove(os.path.join(commit_dir, 'encode/deletion_encode.pkl'))
+                    
+                with open(os.path.join(commit_dir, 'encode/addition_encode.pkl'), 'wb') as file:
+                    pickle.dump(addition_dict, file)    
+                with open(os.path.join(commit_dir, 'encode/deletion_encode.pkl'), 'wb') as file:
+                    pickle.dump(deletion_dict, file)
+                
+                if os.path.exists(os.path.join(commit_dir, 'encode/diff_encode.csv')):
+                    os.remove(os.path.join(commit_dir, 'encode/diff_encode.csv'))
+                if os.path.exists(os.path.join(commit_dir, 'encode/encode_res.pkl')):
+                    os.remove(os.path.join(commit_dir, 'encode/encode_res.pkl'))
+                if os.path.exists(os.path.join(commit_dir, 'encode/simple_encode_res.npz')):
+                    os.remove(os.path.join(commit_dir, 'encode/simple_encode_res.npz'))
+                if os.path.exists(os.path.join(commit_dir, 'encode/simple_encode_res.pkl')):
+                    os.remove(os.path.join(commit_dir, 'encode/simple_encode_res.pkl'))
+                if os.path.exists(os.path.join(commit_dir, 'encode/used_set.pkl')):
+                    os.remove(os.path.join(commit_dir, 'encode/used_set.pkl'))
+                if os.path.exists(os.path.join(commit_dir, 'encode/simple_used_set.pkl')):
+                    os.remove(os.path.join(commit_dir, 'encode/simple_used_set.pkl'))
+            
+            if filename == 'message.txt': # Msg data
+                with open(cur_path, 'r') as file:
+                    msg = file.read()
+                    
+                msg_encode = encoder.encode(msg)
+                with open(os.path.join(commit_dir, 'encode/message_encode.pkl'), 'wb') as file:
+                    pickle.dump(msg_encode, file)
+                
+                # Save the encoded data
+                #diff_df.to_csv(os.path.join(commit_dir, 'encode/diff_encode.csv'), errors='ignore')
+        
+    # Save the vocabulary for each projects
+    if os.path.exists(os.path.join(project_dir, 'vocab.pkl')):
+        os.remove(os.path.join(project_dir, 'vocab.pkl'))
+    
+    with open(os.path.join(project_dir, 'vocab.pkl'), 'wb') as file:
+        pickle.dump(encoder.vocab, file)
+
+if __name__ == "__main__":
+    # Iterate through projects
+    for pid in os.listdir(DIFF_DATA_DIR):
+        print(f'Working on project {pid}')
+        encode_pid(pid)
+        
