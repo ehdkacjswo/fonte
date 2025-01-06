1fbb99bca9f60ae67336868e5ffb7458c0726d9b
ehdkacjswo <ehdkacjswo@gmail.com>
2025-01-06 17:32:29 +0900

    Precise style change detection working

---
+++ precise_style_change/check_astdiff.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+analyzer_jar=/root/workspace/docker/workspace/tools/java_analyzer/target/java-analyzer-1.0-SNAPSHOT-shaded.jar
+
+pid=$1
+vid=$2
+commit_log=$3
+sha=$4
+use_Rewrite=$5
+output=$6
+
+[ -f $output ] && rm $output
+output=$(realpath $output)
+
+workdir=/tmp/${pid}-${vid}b
+[ ! -d $workdir ] && defects4j checkout -p ${pid} -v ${vid}b -w $workdir
+
+cd $workdir
+
+python /root/workspace/precise_style_change/get_touched_files.py $commit_log \
+  --output $workdir/modified_files_${sha} \
+  --commit $sha
+
+echo "- Commit:" $sha
+cat $workdir/modified_files_${sha} | while read after_src_path before_src_path; do
+  echo "-- File:" $after_src_path
+  # checkout to $sha
+  git checkout $sha $after_src_path
+  if [ "$use_Rewrite" = true ]; then
+    java -cp $analyzer_jar analyzer.RewriteRunner $after_src_path cleanup.diff
+    patch -p1 < cleanup.diff
+    rm cleanup.diff
+  fi
+  cp $after_src_path $after_src_path.$sha
+  # checkout to $sha~1
+  git checkout $sha~1 $before_src_path
+  if [ $? -eq 0 ]; then
+    # only when $file is a valid path in $sha~1
+    if [ "$use_Rewrite" = true ]; then
+      java -cp $analyzer_jar analyzer.RewriteRunner $before_src_path cleanup.diff
+      patch -p1 < cleanup.diff
+      rm cleanup.diff
+    fi
+    cp $before_src_path $before_src_path.$sha~1
+
+    # compare AST
+    java -cp $analyzer_jar analyzer.ASTIsomorphicChecker \
+      $after_src_path.$sha $before_src_path.$sha~1 > $after_src_path.$sha.is_isomorphic
+    if [ $? -eq 0 ]; then
+      if grep -Fxq "true" $after_src_path.$sha.is_isomorphic; then
+        result='U'
+      else
+        result='C'
+      fi
+    else
+      result='E' # error
+    fi
+    echo "--- ASTs are Isomorphic (w/ Rewrite: $use_Rewrite):" $(echo $after_src_path.$sha.is_isomorphic)
+
+    rm $after_src_path.$sha
+    rm $before_src_path.$sha~1
+
+    echo $before_src_path,$after_src_path,$result >> $output
+  else
+    rm $after_src_path.$sha
+
+    echo "$before_src_path may not exist in $sha~1"
+    echo "$before_src_path,$after_src_path,N" >> $output
+  fi
+done
+
+echo "* Summary:" $output
+cat $output
+echo ""
+echo ""
---
+++ precise_style_change/combine_style_change_results.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+pid=$1
+vid=$2
+tool=$3
+use_Rewrite=$4
+
+if [ "$use_Rewrite" = true ]; then
+  postfix=""
+else
+  postfix="_noOpenRewrite"
+fi
+
+scdir=/root/workspace/data/Defects4J/core/$pid-${vid}b/$tool/validation${postfix}
+commits=/root/workspace/data/Defects4J/core/${pid}-${vid}b/$tool/commits.pkl
+aggregated=/root/workspace/data/Defects4J/core/${pid}-${vid}b/$tool/precise_validation${postfix}.csv
+
+if [ ! -f $aggregated ]; then
+  touch $aggregated
+  python /root/workspace/docker/workspace/tools/get_candidates.py $commits | while read sha; do
+    if [ -f $scdir/$sha.csv ]; then
+      sed -e "s/^/${sha},/" $scdir/$sha.csv >> $aggregated
+    else
+      rm $aggregated
+      break
+    fi
+  done
+  [ -f $aggregated ] && echo "Successfully generated: $aggregated"
+else
+  echo "$aggregated exists"
+fi
